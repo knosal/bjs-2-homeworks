661252dd42137872fb7c7111993b2d387b90ca62
knosal <jiupuk2015@yandex.ru>
2022-05-15 14:40:59 +0300

    correction in task file (++)

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,5 @@
 "use strict";
 
-/*№1*/ 
-console.log("Задание №1");
 function solveEquation(a, b, c) {
   let arr = [];
   let D = Math.pow(b, 2) - (4 * a * c); 
@@ -24,93 +22,33 @@ function solveEquation(a, b, c) {
     return arr;
   }
 };
-let result = solveEquation();
-console.log(result);
 
-/*
-// D > 0 
-let result1 = solveEquation(1, 5, 4);
-console.log(result1);
-// D = 0 
-let result2 = solveEquation(1, 2, 1);
-console.log(result2);
-// D < 0 
-let result3 = solveEquation(1, 2, 10);
-console.log(result3);
-*/
-
-/*№2*/
-console.log("\nЗадание №2");
 function calculateTotalMortgage(percent, contribution, amount, date) {
 
-  let p = percent;
-  let c = contribution;
-  let a = amount;
-  let d = date;
-  
-//Данные введённые в prompt преобразуются в тип Number.
-  p = prompt("Процент", ""); //процентная ставка (percent)
- // alert(typeof p);
-  
-  alert(typeof p);
-  if (typeof percent == "string") {
-    parseInt(percent);
-    alert(typeof p);
-    if (isNaN(percent) == true) {
-     return console.log(`Параметр <percent> содержит неправильное значение ${percent}`);
-    }
-    
-  } 
-  /*
-  c = +prompt("Взнос", ""); //первоначальный взнос
-  a = +prompt("Кредит", ""); //сумма кредита
-  d = +prompt("Срок", ""); //дата окончания кредита срок в месяцах (n) */
-  
-  let interestRate = p / 100;  //процентная ставка (в %)
-  let S = a - c; //тело кредита (сумма, которую необходимо вернуть банку (сумма кредита минус первоначальный взнос)
+  let creditTerm = date.getMonth() - (new Date()).getMonth() + 12 * (date.getFullYear() - (new Date()).getFullYear()); //срок кредита
+  let interestRate = percent / 100;  //процентная ставка (в %)
+  let S = amount - contribution; //тело кредита (кредит - взнос)
   let P = interestRate / 12; // процентная ставка в месяц
-  let monthlyFee = S * (P + (P / ((Math.pow((1 + P), d) - 1)))); //Ежемесячная оплата
-  let initialPayment = c; //первоначальный взнос
-  let interestLoan = S * interestRate; // сумма процентов по кредиту
-  let totalAmount = monthlyFee * d ; //сумму, которую в итоге заплатит клиент
-
-  console.log("\nТело кредита: " + S.toFixed(2));
-  console.log("Процентная ставка: " + interestRate.toFixed(2));
-  console.log("Сумма процентов по кредиту: " + interestLoan.toFixed(2));
-  console.log("Ежемесячная оплата: " + monthlyFee.toFixed(2));
-  console.log("Срок кредита: " + d + "\n");
+  let monthlyFee = S * (P + (P / ((Math.pow((1 + P), creditTerm) - 1)))); // оплата/мес
+  let totalAmount = monthlyFee * creditTerm; //общая сумма оплаты 
   
-  if (totalAmount === 0) {
-     return totalAmount;
+  if (typeof percent === "string" || Number.isNaN(percent) || percent == Infinity || percent == -Infinity) {
+    return (`Параметр "Процентная ставка" содержит неправильное значение "${percent}"`);
+  };
+
+  if (typeof contribution === "string" || Number.isNaN(contribution) || contribution === Infinity || contribution === -Infinity) {
+    return (`Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`);
+  };
+
+  if (typeof totalAmount === "string" || Number.isNaN(totalAmount) || totalAmount === Infinity || totalAmount === -Infinity) {
+    return (`Параметр "Общая стоимость" содержит неправильное значение "test"`);
+  } else if (typeof percent === "string" || Number.isNaN(percent) || percent == Infinity || percent == -Infinity) {
+    return (`Параметр "Общая стоимость" содержит неправильное значение "${percent}"`);
+  } else if (typeof contribution === "string" || Number.isNaN(contribution) || contribution === Infinity || contribution === -Infinity) {
+    return (`Параметр "Общая стоимость" содержит неправильное значение "${contribution}"`);
+  } else if (totalAmount === 0) {
+      return totalAmount;
   } else {
-     return totalAmount.toFixed(2);
-  }
-}
-//1
-/*let result2 = Number(calculateTotalMortgage(10, 0, 50000, 12));
-console.log("Общая сумма долга: " + result2);
-console.log(typeof result2); //*/
-
-let result2 = Number(calculateTotalMortgage());
-console.log("Общая сумма долга: " + result2);
-console.log(typeof result2); //*/
-/*
-//calculateTotalMortgage(10, 0, 50000, 12);     //52749.53
-calculateTotalMortgage(10, 1000, 50000, 12);  //51694.54
-calculateTotalMortgage(10, 0, 20000, 24);     //22149.56
-calculateTotalMortgage(10, 1000, 20000, 24);  //21042.09
-calculateTotalMortgage(10, 20000, 20000, 24); //0
-calculateTotalMortgage(10, 0, 10000, 36);     //11616.19
-calculateTotalMortgage(15, 0, 10000, 36);     //12479.52*/
-//2
-//calculateTotalMortgage();
-
-
-
-
-
-
-
-
-
-
+      return Number(totalAmount.toFixed(2));
+  };
+};
