d295b6bd741c7206628ab9c74740416bb1529e09
knosal <jiupuk2015@yandex.ru>
2022-05-29 13:40:54 +0300

    all tasks +

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,10 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+  let result = arr1.length == arr2.length && arr1.every((item, i) => item === arr2[i]);
+  return result; 
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
+  let resultArr = arr.filter((i) => i > 0 && i % 3 === 0).map((i) => i * 10);
+  return resultArr; 
 }
+
