96ec14e73dcacc75e55fda7c1b048cbd179be294
knosal <jiupuk2015@yandex.ru>
2022-06-26 22:21:31 +0300

    All tasks

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,68 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];  
+    function wrapper(...args) {
+      const hash = args.join(',');
+      let idx = cache.findIndex((item) => item.hash == hash);
+      if (idx !== -1) {
+        console.log("Из кэша: " + cache[idx].result);
+        return "Из кэша: " + cache[idx].result;
+      }
+  
+      let result = func(...args);
+      cache.push({hash, result});
+      if (cache.length > 5) {
+        cache.shift();
+      }
+      console.log("Вычисляем: " + result);
+      return "Вычисляем: " + result;
+    }
+    return wrapper;
 }
 
+const addThree = (a, b, c) => a + b + c;
+const upgradedAddThree = cachingDecoratorNew(addThree);
+upgradedAddThree(1, 2, 3); // вычисляем: 6
+upgradedAddThree(1, 2, 3); // из кэша: 6
+upgradedAddThree(2, 2, 3); // вычисляем: 7
+upgradedAddThree(3, 2, 3); // вычисляем: 8
+upgradedAddThree(4, 2, 3); // вычисляем: 9
+upgradedAddThree(5, 2, 3); // вычисляем: 10
+upgradedAddThree(6, 2, 3); // вычисляем: 11   (при этом кэш для 1, 2, 3 уничтожается)
+upgradedAddThree(1, 2, 3); // вычисляем: 6  (снова вычисляем, кэша нет)
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(func, ms) {
+  let timer = null;  
+    function wrapper(...args) {
+      if (timer === null) {
+        func(...args);
+      }
+      clearTimeout(timer);
+      timer = setTimeout(() => func(...args), ms);
+    }
+    return wrapper;
 }
 
-function debounceDecorator2(func) {
-  // Ваш код
+const sendSignal = () => console.log("Сигнал отправлен");
+const upgradedSendSignal = debounceDecoratorNew(sendSignal, 2000);
+setTimeout(upgradedSendSignal); // Сигнал отправлен
+setTimeout(upgradedSendSignal, 300); // проигнорировано так как от последнего вызова прошло менее 2000мс (300 - 0 < 2000)
+setTimeout(upgradedSendSignal, 900); // проигнорировано так как времени от последнего вызова прошло: 900-300=600 (600 < 2000)
+setTimeout(upgradedSendSignal, 1200); // проигнорировано так как времени от последнего вызова прошло: 1200-900=300 (300 < 2000)
+setTimeout(upgradedSendSignal, 2300); // проигнорировано так как времени от последнего вызова прошло: 2300-1200=1100 (1100 < 2000)
+setTimeout(upgradedSendSignal, 4400); // Сигнал отправлен так как времени от последнего вызова прошло: 4400-2300=2100 (2100 > 2000)
+setTimeout(upgradedSendSignal, 4500); // Сигнал будет отправлен, так как последний вызов debounce декоратора (спустя 4500 + 2000 = 6500) 6,5с
+
+function debounceDecorator2(func, ms) {
+  let timer = null;
+
+    function wrapper(...args) {
+      if (timer === null) {
+        func(...args);
+      }
+      clearTimeout(timer);
+      timer = setTimeout(() => func(...args), ms);
+      wrapper.count++;
+    }
+    wrapper.count = 0;
+    return wrapper;
 }
